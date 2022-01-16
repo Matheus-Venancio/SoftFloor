16562779a828c80501923b435221d5c9be4c75f6
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-01-16 13:06:33 -0300

    menu finish

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-/**Menu**/
-function abrirMenu() {
-    var menu = document.querySelector("#icon")
-    var nav = document.querySelector('nav')
-    menu.classList.toggle('fa-bars')
-    nav.classList.toggle('active')
-}
+$(document).ready(function() {
+    $('#icon').click(function() {
+        $('ul').toggleClass('show');
+    })
+})
