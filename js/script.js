164d310aeb04653e2045d1c2faba99ab99eb9bf0
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-02-05 02:17:06 -0300

    finish home
    initical project.html

--- js/script.js
+++ js/script.js
@@ -86,3 +86,22 @@ function textInput(evt) {
         if (theEvent.preventDefault) theEvent.preventDefault();
     }
 }
+
+//Effects scrool
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
